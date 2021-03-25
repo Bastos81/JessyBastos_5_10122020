f2e167ee2e0d929e00413d39c6207f524d6d5371
Jessy Bastos <jessybastos.web@gmail.com>
2021-03-25 20:26:18 +0100

    Ajout de la liste d'appareil photo/liens serveur

--- js/index.js
+++ js/index.js
@@ -1,8 +1,48 @@
-$(document).ready(function(){
-    $("#searchInput").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#lessonList .col-12").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
+const searchInput = document.getElementById('search');
+const results = document.getElementById('productList');
+
+let searchTerm = '';
+let cameras;
+
+// API REQUEST
+const fetchCameras = async() => {
+	cameras = await fetch("http://localhost:3000/api/cameras")
+    .then(res => res.json());
+};
+
+const showCameras = async() => {
+	await fetchCameras();
+  
+results.innerHTML = (
+  
+    cameras
+      .filter(camera => camera.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      .map(camera => ( 
+
+        ` 
+            <div class="col-12 col-lg-4">
+                <div class="card camera-item mb-4 border-light shadow-sm">
+                <img class="camera-image card-img-top" src="${camera.imageUrl}" />
+                    <div class="card-body camera-info">
+                        <h5 class="card-title camera-name">${camera.name}</h5>
+                        <p class="card-text camera-description">${camera.description}</p>
+                        <p class="card-text camera-prix">${numberWithCommas(camera.price/100 + " €")}</p>
+                        <a href="#" class="btn btn-primary stretched-link">En savoir plus</a>
+                    </div>
+                </div>
+            </div>
+        `
+      )).join('')
+  );
+}; 
+
+showCameras();
+
+// INPUT SETUP
+searchInput.addEventListener('input', (e) => {searchTerm = e.target.value;
+  showCameras();	
 });
+  
+function numberWithCommas(x){
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
