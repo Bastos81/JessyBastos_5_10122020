dfaf69bc29c01a80e8d0ae18935ee3d80b081955
Jessy Bastos <jessybastos.web@gmail.com>
2021-03-26 12:27:10 +0100

    Avancement page accueil

--- js/test.js
+++ js/test.js
@@ -1,15 +1,19 @@
-const searchInput = document.getElementById("search");
-const results = document.getElementById("productList");
-
-let searchTerm = '';
-let cameras;
+class Cameras {
+    constructor(id, name, description, price, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
 
 const getCameras = async() => {
 	cameras = await fetch("http://localhost:3000/api/cameras")
     .then(res => res.json());
 };
 
-const showCameras = async() => {
+const addCameras = async() => {
 	await getCameras();
   
     results.innerHTML = (
@@ -20,8 +24,8 @@ const showCameras = async() => {
 
             ` 
                 <div class="col-12 col-lg-4">
-                    <div class="card camera-item mb-4 border-light shadow-lg">
-                        <img class="camera-image card-img-top" src="${camera.imageUrl}" />
+                    <div class="card camera-item mb-4 border-light shadow-sm">
+                    <img class="camera-image card-img-top" src="${camera.imageUrl}" />
                         <div class="card-body camera-info">
                             <h5 class="card-title camera-name">${camera.name}</h5>
                             <p class="card-text camera-description">${camera.description}</p>
@@ -36,11 +40,3 @@ const showCameras = async() => {
 }; 
 
 showCameras();
-
-searchInput.addEventListener('input', (e) => {searchTerm = e.target.value;
-  showCameras();	
-});
-  
-function numberWithCommas(x){
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-}
