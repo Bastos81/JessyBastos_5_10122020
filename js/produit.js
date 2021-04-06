444e95d8eb88146b81e8a09f0fe8aac51dbbdca1
Jessy Bastos <jessybastos.web@gmail.com>
2021-04-06 13:14:19 +0200

    Fonction ajout au panier

--- js/produit.js
+++ js/produit.js
@@ -3,8 +3,16 @@ const cameraUrl = new URL(cameraUrl_string);
 const cameraUrlSpace = cameraUrl.searchParams.get("id");
 const cameraUrlId = cameraUrlSpace.trim();
 
-
-
+// Création d'une classe de la caméra avec l'option et la quantité
+class CameraSelected {
+    constructor(idCamera, nameCamera, resultsLensesChoice, quantityCamera, priceCamera) {
+        this.idCamera = idCamera;
+        this.nameCamera = nameCamera;
+        this.resultsLensesChoice = resultsLensesChoice;
+        this.quantityCamera = quantityCamera;
+        this.priceCamera = priceCamera;
+    }
+}
 
 // Affichage de la caméra en fonction de l'Url
 const showChoosenCameras = async() => {
@@ -16,13 +24,13 @@ const showChoosenCameras = async() => {
             <div class="card camera-item mb-4 border-light shadow-lg product-page">
                 <img class="camera-image card-img-top" alt="Appareil Photo Vintage" src="${choosenCamera.imageUrl}" />
                 <div class="card-header bg-dark text-white">
-                    <h1 class="card-title camera-name">${choosenCamera.name}</h1>
+                    <h1 id="camera-selected-name" class="card-title camera-name">${choosenCamera.name}</h1>
                 </div>
                 <div class="card-body camera-info">
                     <p class="card-text camera-description">${choosenCamera.description}</p>
-                    <form>
+                    <form class="needs-validation" novalidate>
                         <div class="alert alert-info alert-dismissible fade show mt-3" role="alert">
-                            <h5 class="alert-heading">N'oubliez pas de choisir votre lentille!</h5>
+                            <h5 class="alert-heading">N'oubliez pas de choisir votre objectif!</h5>
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
@@ -35,12 +43,13 @@ const showChoosenCameras = async() => {
                         </div>
                         <div class="form-group row">
                             <label for="quantity" class="col-sm-2 col-form-label">Quantité</label>
-                            <input class="col-sm-9" type="number" name="quantity" id="quantity" min="1" max="10">
+                            <input class="col-sm-9" type="number" name="quantity" id="quantity" min="1" max="10" value="1">
                         </div>
+                        <p class=" card-text camera-prix font-weight-bold">${numberWithCommas("Prix :"+ " " + choosenCamera.price/100 + " € / unité")}</p>
+                        <a href="../index.html" class="btn btn-dark col-4">Retour à l'accueil</a>
+                        <button id="button-panier" class="btn btn-dark col-4" type="submit">Ajouter au panier</button>
+                        <a href="../html/panier.html" class="my-3 btn btn-dark col-8">Voir mon panier</a>
                     </form>
-                    <p class=" card-text camera-prix font-weight-bold">${numberWithCommas("Prix :"+ " " + choosenCamera.price/100 + " € / unité")}</p>
-                    <a href="../index.html" class="stretched-link btn btn-dark col-4">Retour à l'accueil</a>
-                    <a href="./html/produit.html" class="stretched-link btn btn-dark col-4">Ajouter au panier</a>
                 </div>
             </div>
         </div>
@@ -54,7 +63,33 @@ const showChoosenCameras = async() => {
         resultsLenses.appendChild(optionLens);
         optionLens.textContent = choosenCamera.lenses[i];
     }
+    
+    // Ajout des articles au panier
+    const envoyerPanier = document.getElementById("button-panier");
+
+    envoyerPanier.addEventListener('click', (event) => {event.preventDefault();
+        let productPanier = JSON.parse(localStorage.getItem("productPanier"));
+        let idCamera = choosenCamera._id;
+        let nameCamera = choosenCamera.name;
+        let resultsLensesChoice = document.getElementById('cameraLenses').value;
+        let quantityCamera = document.getElementById('quantity').value;
+        let priceCamera = choosenCamera.price/100*quantityCamera + ",00 €";
+        let product = new CameraSelected (idCamera, nameCamera, resultsLensesChoice, quantityCamera, priceCamera);
+        const popupValidation = window.alert("Le produit a bien été ajouté au panier!");
+
+        if(productPanier){
+            productPanier.push(product);
+            localStorage.setItem("productPanier", JSON.stringify(productPanier));
+        }
+        else{ 
+            productPanier = []
+            productPanier.push(product);
+            localStorage.setItem("productPanier", JSON.stringify(productPanier));  
+        }
+    
+        console.log(productPanier);
+    })
 }; 
 
-
 showChoosenCameras();
+
